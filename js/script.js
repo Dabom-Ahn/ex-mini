81a14ea459b35a4174f4bae035df185177feb843
Dabom Ahn <900428star@naver.com>
2024-09-20 16:38:23 +0900

    margin속성을 absolute의 left로 변경처리하고 css와 js에서 %로 모션 이동 처리

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-//0 : margin-left: 0px; (-400 * 0)
-//1 : margin-left: -400px; (-400 * 1)
-//2 : margin-left: -800px; (-400 * 2)
-//3 : margin-left: -1200px; (-400 * 3)
-//4 : margin-left: -1600px; (-400 * 4)
+//0 : margin-left: 0px; (-400 * 0) :0%
+//1 : margin-left: -400px; (-400 * 1) : -100%
+//2 : margin-left: -800px; (-400 * 2) : -200%
+//3 : margin-left: -1200px; (-400 * 3) : -300%
+//4 : margin-left: -1600px; (-400 * 4) : -400%
 
 const frame = document.querySelector("main");
 const panel = frame.querySelector(".panel");
@@ -11,5 +11,9 @@ const btns = frame.querySelectorAll(".btns li");
 btns.forEach((btn, idx) => {
   btn.addEventListener("click", () => {
     console.log(idx);
+    new Anime(panel, { left: -100 * idx + "%" }, { duration: 500 });
   });
 });
+
+//미션 -  HTML에서 li의 갯수만 수정하면 scss에서 일일이 panel, li의 넓이를 수정할 필요가 없도록 스크립트에서 동적 처리
+//4시 40분까지
